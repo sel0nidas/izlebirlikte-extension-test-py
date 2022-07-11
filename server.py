4b5884bdc8db1f5b3ad6d6fae7e75571b490a579
Selahattin Emre Kutlu <48654612+sel0nidas@users.noreply.github.com>
2022-07-11 13:13:44 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,24 @@
+from websocket_server import WebsocketServer
+
+class Websocket_Server():
+
+    def __init__(self, host, port):
+        self.server = WebsocketServer(host, port)
+
+    def new_client(self, client, server):
+        print("New client({}) connected.".format(client["id"]))
+
+    def client_left(self, client, server):
+        print("Client({}) disconnected.".format(client["id"]))
+
+    def message_received(self, client, server, message):
+        print("Client({}) said,{}".format(client["id"], message))
+
+    def run(self):
+        self.server.set_fn_new_client(self.new_client)
+        self.server.set_fn_client_left(self.client_left)
+        self.server.set_fn_message_received(self.message_received) 
+        self.server.run_forever()
+
+ws_server = Websocket_Server("localhost" , 8080)
+ws_server.run()
